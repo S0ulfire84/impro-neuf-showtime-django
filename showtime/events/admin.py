52004e01821f8033e23185286e0c63beea2a56be
Alejandro C. Saksida <acsaksida@gmail.com>
2023-03-27 20:44:12 +0200

    Adding the app to the settings

--- showtime/events/admin.py
+++ showtime/events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Team, Show, Workshop
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Team)
+admin.site.register(Show)
+admin.site.register(Workshop)
