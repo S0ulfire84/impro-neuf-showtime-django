781566992cc75e973248426924c5d66a2f4d24d9
Alejandro C. Saksida <acsaksida@gmail.com>
2023-03-28 19:37:16 +0200

    Bugfix - There were some compilation errors to deploy to prod

--- showtime/showtime/settings_production.py
+++ showtime/showtime/settings_production.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For Heroku production
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # For Heroku production
 
@@ -140,3 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
