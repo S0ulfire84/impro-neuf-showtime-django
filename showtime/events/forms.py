0326aa2eae65c336e108b819a687f0c04400bbca
Alejandro C. Saksida <acsaksida@gmail.com>
2023-03-30 18:11:00 +0200

    Improve the date fields and add Create Facebook event and TicketCo buttons

--- showtime/events/forms.py
+++ showtime/events/forms.py
@@ -1,15 +1,22 @@
 from django import forms
-from .models import Show, Workshop, Team
+from django.forms import ModelForm, DateTimeInput
+from .models import Show, Workshop, Team, Event
 
 class ShowForm(forms.ModelForm):
     class Meta:
         model = Show
         fields = '__all__'
+        widgets = {
+            'datetime': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 class WorkshopForm(forms.ModelForm):
     class Meta:
         model = Workshop
         fields = '__all__'
+        widgets = {
+            'datetime': DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
 
 class TeamForm(forms.ModelForm):
     class Meta:
